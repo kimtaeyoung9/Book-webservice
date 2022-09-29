c13a5b23e9200007f8d87c16b47761f6e11fdd5e
kimtaeyoung9 <dc_132607@naver.com>
2022-09-29 13:43:19 +0900

    aws 1일차

---
+++ src/main/java/com/mega/book/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.mega.book.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
